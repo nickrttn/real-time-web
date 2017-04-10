77e2f84d46f4726d9f5682264689fa629a717f61
Nick Rutten <hello@nickrttn.me>
2017-04-10 11:27:19 +0200

    Updated server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const chalk = require('chalk');
+
+require('dotenv').config();
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), err => {
+	if (err) throw chalk.red(err); // eslint-disable-line curly
+	console.log(chalk.blue(`----- Listening on http://localhost:${app.get('port')} -----`));
+});
