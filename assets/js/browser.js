551a331850a945ee46596136a65e46b2898b2aed
Nick Rutten <hello@nickrttn.me>
2017-04-12 20:42:11 +0200

    added a little styling to make it look better and display names

--- assets/js/browser.js
+++ assets/js/browser.js
@@ -4,16 +4,26 @@ const io = require('socket.io-client');
 	'use strict';
 	const socket = io();
 	const form = document.querySelector('form');
+	const name = document.querySelector('#name');
 	const msg = document.querySelector('#message');
 	const msgs = document.querySelector('#messages');
 
-	form.addEventListener('submit', (evt) => {
-		socket.emit('chat message', msg.value);
+	form.addEventListener('submit', evt => {
+		socket.emit('chat message', {
+			name: name.value,
+			msg: msg.value
+		});
+
 		evt.preventDefault();
-		form.reset();
+		msg.value = '';
 	});
 
 	socket.on('chat message', msg => {
-		msgs.insertAdjacentHTML('beforeend', `<li>${msg}</li>`);
+		msgs.insertAdjacentHTML('beforeend', `
+			<li data-type="message" data-self=${name.value === msg.name} ?>
+				${msg.msg}
+				<span data-type="sender">${msg.name}</span>
+			</li>`
+		);
 	});
 })();
